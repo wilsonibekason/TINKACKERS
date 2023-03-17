fcd3c98a19e6464d67940710e49306343571ca24
wilsonibekason <wanbekgithub@gmail.com>
2023-03-17 15:59:02 +0100

    add files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1 className="h-40 text-red-800">Hello WORLD</h1>
+    </div>
+  );
+}
+
+export default App;
